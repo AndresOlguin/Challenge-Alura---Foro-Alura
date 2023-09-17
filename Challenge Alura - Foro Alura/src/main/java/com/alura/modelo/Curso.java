5f880114360b8bec3730dedc71c7ade993df8104
Andrés Olguín <134643657+AndresOlguin@users.noreply.github.com>
2023-09-17 14:27:14 -0300

    Add files via upload

---
+++ Challenge Alura - Foro Alura/src/main/java/com/alura/modelo/Curso.java
@@ -0,0 +1,37 @@
+package com.alura.modelo;
+
+public class Curso {
+
+	private Long id;
+	private String nombre;
+	private String categoria;
+
+	public Curso(String nombre, String categoria) {
+		this.nombre = nombre;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
---
+++ Challenge Alura - Foro Alura/src/main/java/com/alura/modelo/StatusTopico.java
@@ -0,0 +1,7 @@
+package com.alura.modelo;
+
+public enum StatusTopico {
+	
+
+
+}
